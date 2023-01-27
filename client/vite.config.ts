69a07c3f1e76f80d0e54b9b142ca368461991480
Justas Rimkus <jusrim3@ktu.lt>
2023-01-27 17:18:34 +0200

    Initial commit

---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import autoprefixer from 'autoprefixer';
+import stylelint from 'vite-plugin-stylelint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), stylelint({ fix: true, customSyntax: 'postcss-scss' })],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
+});
