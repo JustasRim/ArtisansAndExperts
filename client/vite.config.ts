bd33f073be9f0d2498ec441fe69e8ced703efa76
Justas Rimkus <jusrim3@ktu.lt>
2023-02-01 18:29:16 +0200

    Revert "Revert "Revert "using plugin for pwa"""
    This reverts commit 116e193026355df675b8be8bb15d470623044f5b.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,81 +1,11 @@
 import { defineConfig } from 'vite';
-import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 import autoprefixer from 'autoprefixer';
 import stylelint from 'vite-plugin-stylelint';
 
-const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
-  base: '/',
-  includeAssets: ['favicon.svg'],
-  manifest: {
-    name: 'Artisans & Experts',
-    short_name: 'A&E',
-    start_url: '/',
-    theme_color: '#FFF',
-    background_color: '#FFF',
-    orientation: 'portrait',
-    icons: [
-      {
-        src: '/manifest-icon-192.maskable.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: '/manifest-icon-192.maskable.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'maskable',
-      },
-      {
-        src: '/manifest-icon-512.maskable.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: 'icons/manifest-icon-512.maskable.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      },
-    ],
-    display: 'fullscreen',
-  },
-  devOptions: {
-    enabled: process.env.SW_DEV === 'true',
-    /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
-  },
-};
-
-const replaceOptions = { __DATE__: new Date().toISOString() };
-const claims = process.env.CLAIMS === 'true';
-const reload = process.env.RELOAD_SW === 'true';
-const selfDestroying = process.env.SW_DESTROY === 'true';
-
-if (process.env.SW === 'true') {
-  pwaOptions.srcDir = 'src';
-  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts';
-  pwaOptions.strategies = 'injectManifest';
-  (pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest';
-  (pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject';
-}
-
-if (claims) pwaOptions.registerType = 'autoUpdate';
-
-if (reload) {
-  // @ts-expect-error overrides
-  replaceOptions.__RELOAD_SW__ = 'true';
-}
-
-if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), stylelint({ fix: true, customSyntax: 'postcss-scss' }), VitePWA(pwaOptions)],
+  plugins: [react(), stylelint({ fix: true, customSyntax: 'postcss-scss' })],
   build: {},
   css: {
     modules: {
