116e193026355df675b8be8bb15d470623044f5b
Justas Rimkus <jusrim3@ktu.lt>
2023-02-01 18:14:26 +0200

    Revert "Revert "using plugin for pwa""
    This reverts commit ce1a82921509f4c70d0c48a8daebf1d59fda66fb.

--- client/public/serviceWorker.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-
-workbox.routing.registerRoute(({ request }) => request.destination === 'image', new workbox.strategies.NetworkFirst());
