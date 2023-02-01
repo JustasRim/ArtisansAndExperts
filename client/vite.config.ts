ce1a82921509f4c70d0c48a8daebf1d59fda66fb
Justas Rimkus <jusrim3@ktu.lt>
2023-02-01 17:36:55 +0200

    Revert "using plugin for pwa"
    This reverts commit c57051d3eed5bcc0350084b15b2cc2593aee8093.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,53 +1,11 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 import autoprefixer from 'autoprefixer';
 import stylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    stylelint({ fix: true, customSyntax: 'postcss-scss' }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Artisans & Experts',
-        short_name: 'A&E',
-        start_url: '/',
-        theme_color: '#FFF',
-        background_color: '#FFF',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: 'icons/manifest-icon-192.maskable.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'icons/manifest-icon-192.maskable.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'icons/manifest-icon-512.maskable.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'icons/manifest-icon-512.maskable.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-        display: 'fullscreen',
-      },
-    }),
-  ],
+  plugins: [react(), stylelint({ fix: true, customSyntax: 'postcss-scss' })],
   build: {},
   css: {
     modules: {
